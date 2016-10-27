a2a2b86f70582bd157817e5fe959ab97ca8a7cf3
iamsubhranil <subhranilmukherjee190@gmail.com>
2016-10-27 23:58:30 +0530

    Improvements for Converter 1. Broke read and write test into two methods 2. Added primary methods for adding hamming code and detecting error

--- src/com/iamsubhranil/personal/Converter.java
+++ src/com/iamsubhranil/personal/Converter.java
@@ -3,7 +3,6 @@ package com.iamsubhranil.personal;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 
 /**
  * Author : Nil
@@ -13,32 +12,54 @@ import java.util.ArrayList;
 public class Converter {
 
     public static void main(String[] args) {
-        //       Byte b2 = Byte.valueOf("127");
-//        ByteBuffer byteBuffer = ByteBuffer.allocate(20);
-        try {
+        writeTest();
+        readTest();
+    }
 
+    private static void readTest() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("test_tobin.txt");
+            BitStream bitStream = new BitStream();
+            int i = 0;
+            System.out.println("Reading from test_tobin.txt");
+            while ((i = fileInputStream.read()) != -1) {
+                bitStream.addInt(i);
+            }
+            fileInputStream.close();
+            checkAndCorrectBitmap(bitStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void writeTest() {
+        try {
             BitStream bits = new BitStream();
-            FileInputStream fileInputStream = new FileInputStream("summary.txt");
+            FileInputStream fileInputStream = new FileInputStream("test.txt");
             int i;
             System.out.println("Reading file..");
             int bitsize = 0;
-            ArrayList<Integer> backingList = new ArrayList<>();
+//            ArrayList<Integer> backingList = new ArrayList<>();
             while ((i = fileInputStream.read()) != -1) {
                 bits.addInt(i);
-                backingList.add(i);
+                //               backingList.add(i);
                 bitsize++;
             }
             System.out.println("Reading completed..");
             fileInputStream.close();
             int[] counter = {0};
-            System.out.println("Size of bits\nActual : " + bitsize * 8 + "\tRecorded : " + bits.size());
             System.out.println("Size of bytes : " + bits.toBytes().size());
-            System.out.println("Dumping to summary_frombin.txt..");
-            FileOutputStream fileOutputStream = new FileOutputStream("summary_frombin.txt");
+            System.out.println("Adding hamming bits..");
+            addHammingBits(bits);
+            bits.set(7, bits.get(7).complement());
+            System.out.println("Size of bits\nActual : " + bitsize * 8 + "\tRecorded : " + bits.size());
+            System.out.println("Dumping to test_tobin.txt..");
+            FileOutputStream fileOutputStream = new FileOutputStream("test_tobin.txt");
             bits.toBytes().forEach(in -> {
-                if (in != backingList.get(counter[0])) {
-                    System.err.println("Value mismatch at position " + counter[0] + "..\nActual value : " + backingList.get(counter[0]) + "\nSaved value : " + in);
-                }
+                //   if (in != backingList.get(counter[0])) {
+                //        System.err.println("Value mismatch at position " + counter[0] + "..\nActual value : " + backingList.get(counter[0]) + "\nSaved value : " + in);
+                //    }
                 try {
                     fileOutputStream.write(in);
                 } catch (IOException e) {
@@ -52,6 +73,40 @@ public class Converter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void checkAndCorrectBitmap(BitStream bitStream) {
+        int position = 1;
+        System.out.println("Checking error..\n");
+        int prevsize = bitStream.size();
+        BitStream errorAtPosition = new BitStream();
+        while (position < prevsize) {
+            HammingBit hammingBit = new HammingBit(position);
+            hammingBit.setValue(bitStream.get(position));
+            Bit errorCode = hammingBit.decideValueIncludeSelf(bitStream);
+            errorAtPosition.add(errorCode);
+            position = position * 2;
+        }
+        errorAtPosition.forEach(bit -> System.out.printf("%d", bit.getValue()));
+        int maxpower = errorAtPosition.size() - 1;
+        int errint = 0;
+        while (maxpower >= 0) {
+            errint = errint + (errorAtPosition.get(maxpower).getValue() * (1 << maxpower));
+            maxpower--;
+        }
+        System.out.println("\nError at position : " + errint);
+    }
+
+    private static void addHammingBits(BitStream bitStream) {
+        int position = 1;
+        int prevsize = bitStream.size();
+        while (position < prevsize) {
+            HammingBit hammingBit = new HammingBit(position);
+            System.out.printf("%d", hammingBit.decideAndSetValueExcludeSelf(bitStream).getValue());
+            bitStream.add(position - 1, hammingBit);
+            position = position * 2;
+        }
+        System.out.println("\nHamming bits added : " + (bitStream.size() - prevsize));
     }
 
 }
